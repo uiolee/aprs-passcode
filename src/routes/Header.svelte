8e816df880bfca72111b01ae94f90d9cd7fe6b36
uiolee <22849383+uiolee@users.noreply.github.com>
2025-08-21 22:57:55 +0800

    feat(Header): add localization support

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import { Stat } from './shared.svelte';
+
+	import { getLocale, locales, localizeHref, setLocale } from '$lib/paraglide/runtime';
+	import { m } from '$lib/paraglide/messages.js';
+	import { page } from '$app/state';
 </script>
 
 <header>
@@ -8,20 +12,39 @@
 		<ul>
 			<li>
 				<div
-					on:click={() => {
+					onclick={() => {
 						location.replace('/');
 					}}
 				>
-					<strong aria-busy={!Stat.isBusy}>APRS Passcode Calculator</strong>
+					<strong aria-busy={!Stat.isBusy}>{m.title()}</strong>
 				</div>
 			</li>
 		</ul>
-		<ul></ul>
+		<ul>
+			<li>
+				<details class="dropdown">
+					<summary> {m.language()}: <code>{getLocale()}</code></summary>
+					<ul dir="rtl">
+						{#each locales as locale}
+							<li>
+								<a
+									onclick={() => {
+										setLocale(locale);
+										location.replace(localizeHref(page.url.pathname, { locale }));
+									}}
+									href={localizeHref(page.url.pathname, { locale })}><code>{locale}</code></a
+								>
+							</li>
+						{/each}
+					</ul>
+				</details>
+			</li>
+		</ul>
 	</nav>
 </header>
 
 <style>
-	nav ul li div {
-		cursor: pointer;
-	}
+    nav ul li div {
+        cursor: pointer;
+    }
 </style>
