1764f337058fad984589047a8e6ea44d01adcf0c
uiolee <22849383+uiolee@users.noreply.github.com>
2025-08-22 00:09:20 +0800

    l10n(page): add descriptions and title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
+	import { m } from '$lib/paraglide/messages';
 	import AprsCalc from './AprsCalc.svelte';
 </script>
 
 <svelte:head>
-	<title>APRS Passcode</title>
+	<title>{m.title()}</title>
 	<meta
 		name="description"
-		content="a web tool to generate/calculate passcode of aprs for your casllsign."
+		content="{m.description()}"
 	/>
 </svelte:head>
 
